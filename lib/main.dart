a06c5f1b5debb668d1ff5b517b00f35eff4db546
Jeet Singh <jeet29113@gmail.com>
2025-07-14 18:55:43 +0530

    BUILD

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'src/app.dart';
 import 'src/infra/supabase_provider.dart';
 
 void main() async {
   
-  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await initializeSupabase();
   runApp(const ProviderScope(child: App()));
--- lib/src/constants/app_constants.dart
+++ lib/src/constants/app_constants.dart
@@ -1,9 +1,8 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConstants {
-static var supabaseUrl = dotenv.env['SUPABASE_URL']!;
-static var supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY']!;
+  static var supabaseUrl = 'https://xztyceckevvcgmescbls.supabase.co';
+  static var supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh6dHljZWNrZXZ2Y2dtZXNjYmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI0ODY0NzUsImV4cCI6MjA2ODA2MjQ3NX0.emkDE6yYZgz4AX5BTvGIENY-9Dy2Ndhva_5f7RXsSZE';
 
   static const String unknownError = 'Something went wrong. Please try again.';
 }
-
